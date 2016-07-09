5e301d463c5fa26e6a56e028ed4a3040c642d45d
LostInInaka <LostInInaka@users.noreply.github.com>
2016-07-10 00:25:38 +0900

    added version number

--- tewisay.rb
+++ tewisay.rb
@@ -4,6 +4,7 @@ class Tewisay < Formula
   homepage 'https://github.com/neeee/tewisay'
   url 'https://github.com/LostInInaka/tewisay.git'
   sha256 'da7e16048a1682a519d733047830beeefe340a19c9020fbb81ddd9db8d2644ee'
+  version '0.1a'
 
   head 'https://github.com/neeee/tewisay.git'
 
