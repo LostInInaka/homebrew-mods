3beed88305d1d37d3f8cc656e333acc94f406d21
LostInInaka <LostInInaka@users.noreply.github.com>
2016-08-05 20:10:26 +0900

    sha1 --> sha256 (due to depreciation)

--- lcrack.rb
+++ lcrack.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Lcrack < Formula
   homepage 'https://launchpad.net/ubuntu/+archive/'
   url 'https://launchpad.net/ubuntu/+archive/primary/+files/lcrack_20040914.orig.tar.gz'
-  sha1 'e2a3439ab7574f5d1dd345f34f8e244723c42770'
+  sha256 'e2a3439ab7574f5d1dd345f34f8e244723c42770'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
